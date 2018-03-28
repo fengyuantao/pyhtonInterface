2041d23258d48832e5727afb8b8f1c5504ae784b
fyt <815060224@qq.com>
2018-03-28 17:22:15 +0800

    add test_payOrder.py and test some interface

--- Wuliangye/test_case/test_interface.py
+++ Wuliangye/test_case/test_interface.py
@@ -9,20 +9,23 @@ sys.path.append("..")
 from DATA import DATA
 ## 获取验证码码
 #login
-sendCode = requests.post(DATA.sendSecurityCode(),data={'phone':'15680785518'})
+sendCode = requests.post(DATA.sendSecurityCode(),data={'phone':'17761196077'})
 securityCode = raw_input("securityCode:")
-'''
-validateCode = requests.post(DATA.validateCode(),data={'phone':'15680785518','code':securityCode})
+
+
+validateCode = requests.post(DATA.validateCode(),data={'phone':'17761196077','code':securityCode})
+validateStr =  validateCode.json()["data"]
 print validateCode._content
-'''
-
-login = requests.post(DATA.login(),data={'phone':'15680785518','securityCode':securityCode,'terminal':'app','loginDevice':2})
-print login._content
-
-
-
 url = DATA.register()
-data = {'phone':'15680785518','activeCode':'152365','validateStr':securityCode,'terminal':'app','loginDevice':2}
+data = {'phone':'17761196077','activeCode':'914299','validateStr':validateStr,'terminal':'app','loginDevice':2}
 
 result = requests.post(url=url,data=data)
 print result._content
+
+'''
+login = requests.post(DATA.login(),data={'phone':'17761196077','securityCode':securityCode,'terminal':'app','loginDevice':2})
+print login._content
+'''
+
+
+
---
+++ Wuliangye/test_case/test_payOrder.py
@@ -0,0 +1,33 @@
+#coding:utf-8
+import unittest
+from public import Excel
+import ddt
+import requests
+import json
+import time
+import sys
+sys.path.append("..")
+from DATA import DATA
+
+def transfer(method,data):
+    url = DATA.payOrder()
+
+    result = requests.post(url.__dict__[method],data)
+    return result
+
+def wallit(method,data):
+    url = DATA.Wallet()
+    result = requests.post(url.__dict__[method],data)
+    return result
+
+'''
+setPayPassword_data = {"phone":"17761196077","token":"1f7e0db62349483d9f0b43dbd6318985_app","payPassword":"123456"}
+data = {"phone":"17761196077","token":"1f7e0db62349483d9f0b43dbd6318985_app","familyPhone":"15680785518","transferAmount":100,"payPwd":"123456"}
+method = "transferFamily"
+print transfer(method,data)._content
+
+codeCreate_data = {"phone":"17761196077","token":"1f7e0db62349483d9f0b43dbd6318985_app","type":31}
+codeCreate = "codeCreate"
+print transfer(codeCreate,codeCreate_data)._content
+
+'''
